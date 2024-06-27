856a382a0581e2563b97e9991c481af095aa688d
samzhangw <87743920+samzhangw@users.noreply.github.com>
2024-06-28 00:08:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,15 @@ def calculate():
     
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+st.subheader('Best sellers', divider='orange')
+
+''
+''
+
+st.altair_chart(alt.Chart(df)
+    .mark_bar(orient='horizontal')
+    .encode(
+        x='units_sold',
+        y=alt.Y('item_name').sort('-x'),
+    ),
+    use_container_width=True)
